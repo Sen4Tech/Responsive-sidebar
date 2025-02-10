0c5e903fa9658fcff5d1338da6d9fdb36b46d788
Okta-jpg <oktavianuschn@gmail.com>
2025-02-10 14:52:24 +0700

    Feat: Add Menu

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '../views/Home.vue'
 
-const router =  createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            component: Home
-        },
-        {
-            path: '/about',
-            component: About
-        },
-    ]
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/',
+			component: Home
+		},
+		{
+			path: '/about',
+			component: () => import('../views/About.vue')
+		},
+	],
 })
 
 export default router
