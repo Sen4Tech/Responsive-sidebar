4da217418f26180f8e1f0fec9055e382fdaebd7a
Okta-jpg <oktavianuschn@gmail.com>
2025-02-20 09:48:54 +0700

    feat: Add table Atribut

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,21 @@
 import { ref } from 'vue';
 
 const data = ref([
-  ['Data 1', 'Data 2', 'Data 3'],
-  ['Data 4', 'Data 5', 'Data 6']
+  ['Data 1', 'Data 2', 'Data 3', 'Data 4'],
+  ['Data 4', 'Data 5', 'Data 6', 'Data 7']
 ]);
 </script>
 <template>
     <main class="Home-page flex flex-col">
-        <h1 class="font-bold text-2xl flex">Data From Progress</h1>
+        <h1 class="font-bold text-2xl flex leading-15">Data From Progress</h1>
         <div class="container">
     <table class="table border border-2 font-medium w-150 h-50">    
       <thead class="">
         <tr>
-          <th class="border-left border-2">Kolom 1</th>
-          <th class="border-left border-2">Kolom 2</th>
-          <th class="border-left border-2">Kolom 3</th>
+          <th class="border-left border-2">No Do</th>
+          <th class="border-left border-2">Kategori</th>
+          <th class="border-left border-2">Nama Barang</th>
+          <th class="border-left border-2">Harga Satuan</th>
         </tr>
       </thead>
       <tbody class="text-center border border-2">
@@ -23,6 +24,7 @@ const data = ref([
           <td class="border-left border-2">{{ row[0] }}</td>
           <td class="border-left border-2">{{ row[1] }}</td>
           <td class="border-left border-2">{{ row[2] }}</td>
+          <td class="border-left border-2">{{ row[3] }}</td>
         </tr>
       </tbody>
     </table>
