f5478a3f58c728ec787e98c20db6c769dad9dfb1
okta <oktavianuschn@gmail.com>
2025-02-24 13:35:21 +0700

    Feat: Modify Tablebot

--- src/components/pages/Tablebot.vue
+++ src/components/pages/Tablebot.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import axios from 'axios';
+import { Edit, Trash } from 'lucide-vue-next'; 
 
 const collection = ref([]);
 
@@ -21,28 +22,35 @@ onMounted(getData);
 <template>
     <main class="Home-page flex flex-col">
         <h1 class="font-bold text-2xl flex leading-15">Data From Progress</h1>
-        <div class="container">
-    <table class="table border border-2 font-medium w-250 h-30">    
-      <thead>
+        
+        <div class="container overflow-x-auto">
+    <table class="w-270 table-auto border-collapse bg-white shadow-lg rounded-lg">    
+      <thead class="bg-gray-100 text-gray-700">
         <tr>
-          <th class="border-left border-2">No Contract</th>
-          <th class="border-left border-2">Kode Customer</th>
-          <th class="border-left border-2">Nama Customer</th>
-          <th class="border-left border-2">Nama Divisi</th>
-          <th class="border-left border-2">Nama Sub Divisi</th>
-          <th class="border-left border-2">Item No</th>
-          <th class="border-left border-2">Description</th>
+          <th class="px-4 py-3 text-left border-b font-semibold">No Contract</th>
+          <th class="px-4 py-3 text-left border-b font-semibold">Kode Customer</th>
+          <th class="px-4 py-3 text-left border-b font-semibold">Nama Customer</th>
+          <th class="px-4 py-3 text-left border-b font-semibold">Nama Divisi</th>
+          <th class="px-4 py-3 text-left border-b font-semibold">Nama Sub Divisi</th>
+          <th class="px-4 py-3 text-left border-b font-semibold">Item No</th>
+          <th class="px-4 py-3 text-left border-b font-semibold">Description</th>
+          <th class="px-4 py-3 text-left border-b font-semibold">Actions</th> 
         </tr>
       </thead>
-      <tbody class="text-center border border-2 ">
+
+      <tbody class="text-gray-700">
         <tr class="text-black">
-          <td class="border-left border-2">{{ collection.No_Contract }}</td>
-          <td class="border-left border-2">{{ collection.Cust_KD }}</td>
-          <td class="border-left border-2">{{ collection.cust_name }}</td>
-          <td class="border-left border-2">{{ collection.divisi_nm}}</td>
-          <td class="border-left border-2">{{ collection.sub_divisi_nm}}</td>
-          <td class="border-left border-2">{{ collection.itemno}}</td>
-          <td class="border-left border-2">{{ collection.item_desc}}</td>
+          <td class="px-4 py-3 border-b">{{ collection.No_Contract }}</td>
+          <td class="px-4 py-3 border-b">{{ collection.Cust_KD }}</td>
+          <td class="px-4 py-3 border-b">{{ collection.cust_name }}</td>
+          <td class="px-4 py-3 border-b">{{ collection.divisi_nm}}</td>
+          <td class="px-4 py-3 border-b">{{ collection.sub_divisi_nm}}</td>
+          <td class="px-4 py-3 border-b">{{ collection.itemno}}</td>
+          <td class="px-4 py-3 border-b">{{ collection.item_desc}}</td>
+          <td class="px-4 py-3 border-b text-gray-500 flex justify-start space-x-2"> 
+              <Edit class="cursor-pointer hover:text-blue-500" size="18"/> 
+              <Trash class="cursor-pointer hover:text-red-500" size="18"/> 
+            </td>
         </tr>
       </tbody>
     </table>
