6c85554bad35693cea40eb15811d18e302a1f174
Okta-jpg <oktavianuschn@gmail.com>
2025-02-26 08:44:35 +0700

    Feat: Fix Dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx,vue}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['InterVariable', '...defaultTheme.fontFamily.sans'],
-      },
-    },
+    extend: {},
   },
-  // ...
+  plugins: [],
 }
